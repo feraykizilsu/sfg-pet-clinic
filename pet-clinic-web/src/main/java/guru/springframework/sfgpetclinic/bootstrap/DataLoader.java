bae4457a20f7d3d7025b3a37c8637568d8b75a91
feraykizilsu <feraykizilsu@gmail.com>
2021-01-27 22:28:18 +0100

    added DataLoader class for Services and connected Controllers and Pages

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,45 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
+import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    @Autowired
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner = new Owner();
+        owner.setFirstName("Michael");
+        owner.setLastName("Weston");
+
+        ownerService.save(owner);
+
+        Owner owner2 = new Owner();
+        owner2.setFirstName("Piet");
+        owner2.setLastName("Weston");
+        ownerService.save(owner2);
+
+        Vet vet = new Vet();
+        vet.setFirstName("Sam");
+        vet.setLastName("Axe");
+
+        vetService.save(vet);
+
+    }
+}
